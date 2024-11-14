3d139af45fea9b0a8c9113dc0070d0586901b5e6
Eric Dennison <ericd@netdenizen.com>
2024-11-14 17:37:49 -0500

    Latest work
    Unable to get this working through stdin and stdout

--- telnet_server.gd
+++ telnet_server.gd
@@ -15,10 +15,10 @@ const REQUIRED_FEATURES = [
 	39,
 	255,
 	254,
-	1,   # don't echo
+	1,  # don't echo
 	255,
 	251,
-	1,   # will echo
+	1,  # will echo
 	255,
 	251,
 	3,
@@ -33,15 +33,29 @@ const REQUIRED_FEATURES = [
 var forth: AMCForth
 var connection: StreamPeerTCP = null
 var negotiation_complete := false
-var output_buffer := ""
+var output_buffer := ""  # output from pForth
+var input_buffer := ""  # input to pForth
+
+# pForth
+var _pipe
+var _thread
+var _info
 
 @onready var server: TCPServer = TCPServer.new()
 
 
 func _ready() -> void:
 	_start_listening()
-	forth = AMCForth.new()
-	forth.connect("terminal_out", _on_forth_output)
+	#forth = AMCForth.new()
+	#forth.connect("terminal_out", _on_forth_output)
+	# Tap into pForth
+	_info = OS.execute_with_pipe("pforth.exe", PackedStringArray([]))
+	if _info.size():
+		print("pforth.exe running")
+		_pipe = _info["stdio"]
+		_thread = Thread.new()
+		_thread.start(_thread_func)
+		get_window().close_requested.connect(clean_func)
 
 
 func _process(_delta: float) -> void:
@@ -50,7 +64,7 @@ func _process(_delta: float) -> void:
 			connection = server.take_connection()
 			server.stop()  # do not listen now
 			connection.set_no_delay(true)
-			forth.client_connected()
+			#forth.client_connected()
 	else:  # not listening.. connected
 		if connection:
 			var connect_status = connection.get_status()
@@ -76,7 +90,9 @@ func _process(_delta: float) -> void:
 					# just retrieve the text and pass to forth
 					else:
 						var instr: String = raw_data[1].get_string_from_ascii()
-						forth.terminal_in(instr)
+						var ascii: PackedByteArray = instr.to_ascii_buffer()
+						input_buffer += ascii.get_string_from_ascii()
+						_on_forth_input.call_deferred(input_buffer)
 
 
 func _start_listening() -> void:
@@ -88,4 +104,35 @@ func _start_listening() -> void:
 
 
 func _on_forth_output(text: String) -> void:
-	output_buffer += text
+	for c in text.to_ascii_buffer():
+		if c == 10:
+			output_buffer += char(13)  # add a cr to the LF
+		output_buffer += char(c)
+
+
+func _on_forth_input(text: String) -> void:
+	var buffer = text.to_utf8_buffer()
+	_pipe.seek_end()
+	#print("Error: ", _pipe.get_error())
+	_pipe.store_buffer(buffer)
+	#_pipe.store_line(text)
+	#print("Error: ", _pipe.get_error())
+	# print("file position: ", _pipe.get_position())  # FIXME always zero
+	#print("Error: ", _pipe.get_error())
+	print("sending to pipe: ", text)
+	for c in buffer:
+		print(c)
+	input_buffer = ""
+
+
+func _thread_func():
+	while _pipe.is_open() and _pipe.get_error() == OK:
+		_on_forth_output.call_deferred(char(_pipe.get_8()))
+		if not input_buffer.is_empty():
+			_on_forth_input.call_deferred(input_buffer)
+	printerr("pipe is broken")
+
+
+func clean_func():
+	_pipe.close()
+	_thread.wait_to_finish()
